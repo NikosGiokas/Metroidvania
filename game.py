7f596eac83aaaaf8b6dbbe4db40436b0c5411b1c
Nikolaos Giokas <Nikosgiokas@icloud.com>
2025-12-20 22:04:59 +0200

    cleaned up code

--- game.py
+++ game.py
@@ -22,37 +22,25 @@ player = Player()
 
 dv = 0.5
 gravity = 0.02
-
-
-
-
-
-
-
-
-
-# Game loop
 running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == KEYDOWN:  
-            if event.key == K_a:  
-                player.leftMovement = dv 
-            if event.key == K_d:  
-                player.rightMovement = dv  
-            if event.key == K_SPACE and player.standing == True:
-                player.jumping = True
-        if event.type == KEYUP:
-            if event.key == K_a:
-                player.leftMovement = 0
-            if event.key == K_d:  
-                player.rightMovement = 0
-            if event.key == K_SPACE:
-                player.jumping = False
 
+def give_movement(player: Player, event: pygame.Event):
+    if event.type == KEYDOWN:  
+        if event.key == K_a:  
+            player.leftMovement = dv 
+        if event.key == K_d:  
+            player.rightMovement = dv  
+        if event.key == K_SPACE and player.standing == True:
+            player.jumping = True
+    if event.type == KEYUP:
+        if event.key == K_a:
+            player.leftMovement = 0
+        if event.key == K_d:  
+            player.rightMovement = 0
+        if event.key == K_SPACE:
+            player.jumping = False
 
+def limit_out_of_bounds(player: Player):
     if player.x < 0: 
         player.x = 1
     elif player.x > 900:
@@ -62,34 +50,38 @@ while running:
     elif player.y > 500:
         player.y = 500
 
-    player.x -= player.leftMovement
-    player.x += player.rightMovement
+def apply_vertical_movement(player: Player):
     if player.y >= 499 and player.y <= 504:
         player.standing = True
     else:
         player.standing = False
-    
+
     if player.standing == False:
         player.velocity += gravity
     elif player.standing == True:
         player.velocity = 0
     if player.jumping == True and player.standing == True:
         player.velocity = -3
-    
-    
-    
-    print(player.velocity)
     player.y += player.velocity
-    
-    
-    
+
+def apply_horizontal_movement(player: Player):
+    player.x -= player.leftMovement
+    player.x += player.rightMovement
+
+# Game loop
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        give_movement(player, event)
+    limit_out_of_bounds(player)
+    apply_vertical_movement(player)
+    apply_horizontal_movement(player)
     
     screen.fill((30,200,50))
     pygame.draw.rect(screen,(255,0,0),[player.x,player.y,100,100],0)
     pygame.display.update()
-
-
-
 
 # Quit Pygame
 pygame.quit()
