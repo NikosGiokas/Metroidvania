f5b8760ef3cde789b4eb6c8ef457b5cbe0cd4e64
Nikos Giokas <Nikosgiokas@icloud.com>
2025-12-20 22:37:34 +0200

    cleaned code further and made screen size variable

--- game.py
+++ game.py
@@ -4,9 +4,10 @@ from sys import exit
 
 # Initialize Pygame
 pygame.init()
-
+screenWidth = 1600
+screenHeight = 1000
 # Set up the game window
-screen = pygame.display.set_mode((1000, 600))
+screen = pygame.display.set_mode((screenWidth, screenHeight))
 pygame.display.set_caption("Metroidvania")
 
 class Player:
@@ -18,18 +19,19 @@ class Player:
     rightMovement = 0
     standing = False
     jumping = False
-player = Player()
+    height = 100
+    width = 100
 
-dv = 0.5
-gravity = 0.02
+DV = 0.5
+GRAVITY = 9.81 / 500
 running = True
 
 def give_movement(player: Player, event: pygame.Event):
     if event.type == KEYDOWN:  
         if event.key == K_a:  
-            player.leftMovement = dv 
+            player.leftMovement = DV 
         if event.key == K_d:  
-            player.rightMovement = dv  
+            player.rightMovement = DV  
         if event.key == K_SPACE and player.standing == True:
             player.jumping = True
     if event.type == KEYUP:
@@ -43,25 +45,27 @@ def give_movement(player: Player, event: pygame.Event):
 def limit_out_of_bounds(player: Player):
     if player.x < 0: 
         player.x = 1
-    elif player.x > 900:
-        player.x = 900 
+    elif player.x > screenWidth-player.width:
+        player.x = screenWidth-player.width 
     elif player.y < 0:
         player.y = 0 
-    elif player.y > 500:
-        player.y = 500
+    elif player.y > screenHeight-player.height:
+        player.y = screenHeight-player.height
 
 def apply_vertical_movement(player: Player):
-    if player.y >= 499 and player.y <= 504:
+    height_offset = 5
+    jump_velocity = -3
+    if player.y >= screenHeight-player.height-height_offset and player.y <= screenHeight-player.height+height_offset:
         player.standing = True
     else:
         player.standing = False
 
     if player.standing == False:
-        player.velocity += gravity
+        player.velocity += GRAVITY
     elif player.standing == True:
         player.velocity = 0
     if player.jumping == True and player.standing == True:
-        player.velocity = -3
+        player.velocity = jump_velocity
     player.y += player.velocity
 
 def apply_horizontal_movement(player: Player):
@@ -70,6 +74,7 @@ def apply_horizontal_movement(player: Player):
 
 # Game loop 
 
+player = Player()
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -80,7 +85,7 @@ while running:
     apply_horizontal_movement(player)
     
     screen.fill((30,200,50))
-    pygame.draw.rect(screen,(255,0,0),[player.x,player.y,100,100],0)
+    pygame.draw.rect(screen,(255,0,0),[player.x,player.y,player.width,player.height],0)
     pygame.display.update()
 
 # Quit Pygame
